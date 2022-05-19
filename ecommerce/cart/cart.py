05599e0236a0eda9d95866c3c284b6137a47488b
kei_kusanagi <kei_kusanagi@outlook.com>
2022-05-19 18:56:30 -0500

    mostrar precio correcto y thumbnails en el carrito

--- ecommerce/cart/cart.py
+++ ecommerce/cart/cart.py
@@ -16,6 +16,11 @@ class Cart(object):
         for p in self.cart.keys():
             self.cart[str(p)]['product'] = Product.objects.get(pk=p)
 
+        for item in self.cart.values():
+            item['total_price'] = int(item['product'].price * item['quantity']) / 100
+
+            yield item
+
     def __len__(self):
         return sum(item['quantity'] for item in self.cart.values())
 
