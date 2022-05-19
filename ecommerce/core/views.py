b49b4759f4b25972610fd4c497f8187f1de3815c
kei_kusanagi <kei_kusanagi@outlook.com>
2022-05-19 15:26:30 -0500

    creando Singin, login y logout

--- ecommerce/core/views.py
+++ ecommerce/core/views.py
@@ -1,17 +1,32 @@
+from django.contrib.auth import login
 from django.db.models import Q
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from product.models import Product, Category
 
+from .forms import SignUpForm
 # Create your views here.
+
 def frontpage(request):
     products = Product.objects.all()[0:8]
 
     return render(request, 'core/frontpage.html', {'products': products})
 
 def signup(request):
-    return render(request, 'core/signup.html')
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
 
-def login(request):
+        if form.is_valid():
+            user = form.save()
+
+            login(request, user)
+
+            return redirect('/')
+    else:
+        form = SignUpForm()
+
+    return render(request, 'core/signup.html', {'form': form})
+
+def login_old(request):
     return render(request, 'core/login.html')
 
 
