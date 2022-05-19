19c645cea402f149fc5ed6de5f29aec2e7f54f2d
kei_kusanagi <kei_kusanagi@outlook.com>
2022-05-19 16:55:41 -0500

    error en signup

--- ecommerce/core/forms.py
+++ ecommerce/core/forms.py
@@ -1,11 +1,10 @@
-import email
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 class SignUpForm(UserCreationForm):
     first_name = forms.CharField(max_length=50, required=True)
-    lasst_name = forms.CharField(max_length=50, required=True)
+    last_name = forms.CharField(max_length=50, required=True)
     email = forms.EmailField(max_length=255, required=True)
 
     class Meta:
