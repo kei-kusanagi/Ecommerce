999f2e079fdd81e26d388badf8792d1b5c7a0394
kei_kusanagi <kei_kusanagi@outlook.com>
2022-05-19 19:07:39 -0500

    checar estar logeado para hacer checkout

--- ecommerce/cart/views.py
+++ ecommerce/cart/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 # Create your views here.
@@ -10,8 +11,14 @@ def add_to_cart(request, product_id):
     return render(request, 'cart/menu_cart.html')
 
 def cart(request):
+
+    # cart = Cart(request)
+    # print(cart)
+    # for item in cart:
+    #     print(item)
     return render(request, 'cart/cart.html')
 
+@login_required
 def checkout(request):
     return render(request, 'cart/checkout.html')
     
