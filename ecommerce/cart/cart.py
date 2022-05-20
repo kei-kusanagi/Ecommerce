b13424b9d5834a66365b15af3e5af8d0270bb0c3
kei_kusanagi <kei_kusanagi@outlook.com>
2022-05-19 20:03:33 -0500

    ver informacion de usuario en el checkout

--- ecommerce/cart/cart.py
+++ ecommerce/cart/cart.py
@@ -20,32 +20,35 @@ class Cart(object):
             item['total_price'] = int(item['product'].price * item['quantity']) / 100
 
             yield item
-
+    
     def __len__(self):
         return sum(item['quantity'] for item in self.cart.values())
-
+    
     def save(self):
         self.session[settings.CART_SESSION_ID] = self.cart
         self.session.modified = True
-
-    def add(self, product_id, queantity=1, update_quantity=False):
+    
+    def add(self, product_id, quantity=1, update_quantity=False):
         product_id = str(product_id)
 
         if product_id not in self.cart:
             self.cart[product_id] = {'quantity': 1, 'id': product_id}
-
+        
         if update_quantity:
-            self.cart[product_id]['quantity'] += int(queantity)
+            self.cart[product_id]['quantity'] += int(quantity)
 
             if self.cart[product_id]['quantity'] == 0:
                 self.remove(product_id)
-        
+            
         self.save()
     
     def remove(self, product_id):
         if product_id in self.cart:
             del self.cart[product_id]
             self.save()
-
+    
     def get_total_cost(self):
-            return sum(item['total_price'] for item in self.cart.values())
+        for p in self.cart.keys():
+            self.cart[str(p)]['product'] = Product.objects.get(pk=p)
+
+        return int(sum(item['product'].price * item['quantity'] for item in self.cart.values())) / 100
--- ecommerce/ecommerce/urls.py
+++ ecommerce/ecommerce/urls.py
@@ -5,6 +5,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('core.urls')),
-    path('cart', include('cart.urls')),
+    path('cart/', include('cart.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
