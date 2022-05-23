549eb823c711657f81dbacae4aab6e72b425b8d6
kei_kusanagi <kei_kusanagi@outlook.com>
2022-05-23 18:42:26 -0500

    create view and url for update cart

--- ecommerce/cart/views.py
+++ ecommerce/cart/views.py
@@ -4,6 +4,8 @@ from django.shortcuts import render
 # Create your views here.
 from .cart import Cart
 
+from product.models import Product
+
 def add_to_cart(request, product_id):
     cart = Cart(request)
     cart.add(product_id)
@@ -17,6 +19,33 @@ def cart(request):
     # for item in cart:
     #     print(item)
     return render(request, 'cart/cart.html')
+
+def update_cart(request, product_id, action):
+    cart = Cart(request)
+
+    if action == 'increment':
+        cart.add(product_id, 1, True)
+    else:
+        cart.add(product_id, -1, True)
+    product = Product.objects.get(pk=product_id)
+    quantity = cart.get_item(product_id)
+
+    item = {
+        'product': {
+            'id': product.id,
+            'name': product.name,
+            'image':product.image,
+            'get_thumbnail': product.get_thumbnail(),
+            'price':product.price,
+        },
+        'total_price': (quantity * product.price) / 100,
+        'quantity': quantity,
+    }
+
+    response = render(request, 'cart/partials/cart_item.html', {'item': item})
+    response['HX-Trigger'] = 'update-menu-cart'
+
+    return response
 
 @login_required
 def checkout(request):
