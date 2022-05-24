866ac3ef45be2b7e0dd04f825097bfe7bb2922a7
kei_kusanagi <kei_kusanagi@outlook.com>
2022-05-23 19:09:24 -0500

    add htmx attributes for cart buttons

--- ecommerce/cart/urls.py
+++ ecommerce/cart/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from cart.views import add_to_cart, cart, checkout, hx_menu_cart
+from cart.views import add_to_cart, cart, checkout, hx_menu_cart, update_cart
 
 urlpatterns = [
     path('', cart, name='cart'),
     path('checkout/', checkout, name='checkout'),
     path('add_to_cart/<int:product_id>/', add_to_cart, name='add_to_cart'),
+    path('update_cart/<int:product_id>/<str:action>/', update_cart, name='update_cart'),
     path('hx_menu_cart/', hx_menu_cart, name='hx_menu_cart'),
 ]
     
--- ecommerce/cart/views.py
+++ ecommerce/cart/views.py
@@ -28,7 +28,7 @@ def update_cart(request, product_id, action):
     else:
         cart.add(product_id, -1, True)
     product = Product.objects.get(pk=product_id)
-    quantity = cart.get_item(product_id)
+    quantity = cart.get_item(product_id)['quantity']
 
     item = {
         'product': {
