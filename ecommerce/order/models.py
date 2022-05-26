37d23b2bc4e1f0fff18181f04befb2cba110ad5c
kei_kusanagi <kei_kusanagi@outlook.com>
2022-05-26 15:45:59 -0500

    show correct prices in the orders page

--- ecommerce/order/models.py
+++ ecommerce/order/models.py
@@ -32,8 +32,18 @@ class Order(models.Model):
     class Meta:
         ordering = ('-created_at',)
 
+    def get_total_price(self):
+        if self.paid_amount:
+            return self.paid_amount / 100
+        
+        return 0
+        
+
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, related_name='items', on_delete=models.CASCADE)
     product = models.ForeignKey(Product, related_name='items', on_delete=models.CASCADE)
     price = models.IntegerField()
     quantity = models.IntegerField(default=1)
+
+    def get_total_price(self):
+        return self.price / 100
