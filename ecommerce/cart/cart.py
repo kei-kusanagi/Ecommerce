6b477f1975a5b2190f52933636e25a32876b7d4e
kei_kusanagi <kei_kusanagi@outlook.com>
2022-05-26 15:28:10 -0500

    clear cart after purcharse

--- ecommerce/cart/cart.py
+++ ecommerce/cart/cart.py
@@ -46,6 +46,10 @@ class Cart(object):
         if product_id in self.cart:
             del self.cart[product_id]
             self.save()
+
+    def clear(self):
+        del self.session[settings.CART_SESSION_ID]
+        self.session.modified = True
     
     def get_total_cost(self):
         for p in self.cart.keys():
--- ecommerce/order/views.py
+++ ecommerce/order/views.py
@@ -64,4 +64,6 @@ def start_order(request):
 
         item = OrderItem.objects.create(order=order, product=product, price=price, quantity=quantity)
 
+    cart.clear()
+
     return JsonResponse({'session': session, 'order': payment_intent})
