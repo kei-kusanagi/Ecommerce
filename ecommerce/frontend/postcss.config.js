97406955a74a34d474132e5a7f7de74c54815ccd
kei_kusanagi <kei_kusanagi@outlook.com>
2022-06-06 20:38:23 -0500

    add webpack

---
+++ ecommerce/frontend/postcss.config.js
@@ -0,0 +1,12 @@
+const postcssPresetEnv = require("postcss-preset-env");
+
+module.exports = {
+  plugins: [
+    require('postcss-import'),
+    require('tailwindcss/nesting')(require('postcss-nesting')),
+    require('tailwindcss'),
+    require('postcss-preset-env')({
+      features: { 'nesting-rules': false }
+    }),
+  ]
+};
---
+++ ecommerce/frontend/tailwind.config.js
@@ -0,0 +1,35 @@
+const Path = require("path");
+const pwd = process.env.PWD;
+console.log('current path ${pwd}');
+const currentPath = Path.join(pwd, "../core/templates/**/*.html");
+
+// We can add current project paths here
+const projectPaths = [
+    currentPath
+    // add js file paths if you need
+];
+
+const contentPaths = [...projectPaths];
+console.log('tailwindcss will scan ${contentPaths}');
+
+module.exports = {
+    content: contentPaths,
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
+
+// module.exports = {
+//   content: [
+//   './**/*.{html,js}',
+//   './../core/templates/**/*.{html,js}',
+//   '!./node_modules',
+//   '!./build',
+// ],
+  
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
