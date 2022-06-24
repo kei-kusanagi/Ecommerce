4b4ae5fc50920e206c3af9ba3791b21df99139ed
Hector De Diego <lecksfrawen@gmail.com>
2022-06-23 21:12:56 -0500

    New dark mode (#3)
    * fix: remove static references
    
    * fix: webpack configuration and asset files
    
    * fix: missing imports in the template
    
    * fix: env and requirements
    
    * chore: update db
    
    * fix: loading of dark theme script

--- ecommerce/frontend/tailwind.config.js
+++ ecommerce/frontend/tailwind.config.js
@@ -1,20 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+
 const Path = require("path");
+
 const pwd = process.env.PWD;
-console.log('current path ${pwd}');
-const currentPath = Path.join(pwd, "../core/templates/**/*.html");
+console.log(`current path ${pwd}`);
+// const currentPath = Path.join(pwd, "../core/templates/**/*.html");
 
 // We can add current project paths here
 const projectPaths = [
-    currentPath,
-    "../core/static/**/*",
+    // currentPath,
+    "../core/templates/core/*.html",
+    "../core/static/css/*",
+    "../core/static/js/*",
+    "../build/js/*",
     // add js file paths if you need
 ];
 
 const contentPaths = [...projectPaths];
-console.log('tailwindcss will scan ${contentPaths}');
+console.log(`tailwindcss will scan ${contentPaths}`);
 
 module.exports = {
-    content: contentPaths,
+    purge: contentPaths,
+    // content: contentPaths,
     darkMode: 'class',
     theme: {
         extend: {},
