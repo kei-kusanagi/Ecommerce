0c89ea2e5b41fb9400c36dadd5e6e32f5681ebd3
kei_kusanagi_99 <kei_kusanagi@outlook.com>
2022-09-02 15:37:19 -0500

    fixed problem to migrate

--- ecommerce/ecommerce/urls.py
+++ ecommerce/ecommerce/urls.py
@@ -13,5 +13,14 @@ urlpatterns = [
     path('cart/', include('cart.urls')),
     path('order/', include('order.urls')),
     path('admin/', admin.site.urls),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
-+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+# urlpatterns = [
+#     path('__debug__/', include('debug_toolbar.urls')),
+     
+#     path('', include('core.urls')),
+#     path('cart/', include('cart.urls')),
+#     path('order/', include('order.urls')),
+#     path('admin/', admin.site.urls),
+# ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
+# + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
