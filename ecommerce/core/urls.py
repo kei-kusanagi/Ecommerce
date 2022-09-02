f85cd1193cb891a9c2b4a7b239bf9521ed343868
kei_kusanagi_99 <kei_kusanagi@outlook.com>
2022-09-02 18:00:54 -0500

    fix procfile

--- ecommerce/core/urls.py
+++ ecommerce/core/urls.py
@@ -9,7 +9,7 @@ from contact.views import contact
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('', frontpage, name='frontpage'),
+    
     path('', TemplateView.as_view(template_name="index.html")),  
     path('signup/', signup, name='signup'),
     path('logout/', views.LogoutView.as_view(), name='logout'),
